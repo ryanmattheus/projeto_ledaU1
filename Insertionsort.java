e0a4b4ed1b9020f0c94a1074d5b92ab91c314fba
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2026-06-07 18:57:24 -0300

    Insertionsort ops

---
+++ Insertionsort.java
@@ -0,0 +1,16 @@
+public class Insertionsort {
+    // Versão do slide
+    public static void sort(Estudante[] A) {
+        int n = A.length;
+        for (int j = 1; j < n; j++) {
+            Estudante chave = A[j];
+            int i = j - 1;
+            // Desloca elementos maiores que a chave uma posição à direita
+            while (i >= 0 && A[i].compareTo(chave) > 0) {
+                A[i + 1] = A[i];
+                i--;
+            }
+            A[i + 1] = chave;
+        }
+    }
+}
